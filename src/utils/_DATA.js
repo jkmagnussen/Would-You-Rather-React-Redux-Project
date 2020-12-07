5b625a5550d5ee07a0a71af27d0aa34febb0207c
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-12-07 21:31:08 +0000

    fully working mock

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -1,9 +1,21 @@
+import Lauryn from "../media/Lauryn.jpg"
+import Erik from "../media/Erik.jpg"
+import Joe from "../media/Joe.jpg"
+import Cal from "../media/Cal.jpg"
+import Annie from "../media/Annie.jpg"
+import Mike from "../media/Mike.jpg"
+import Matt from "../media/Matt.jpg"
+import Jen from "../media/Jen.jpg"
+import Danni from "../media/Danni.jpg"
+import Ian from "../media/Ian.jpg"
+
+
+
 let users = {
-  sarahedo: {
-    id: "sarahedo",
-    name: "Sarah Edo",
-    avatarURL:
-      "https://pbs.twimg.com/profile_images/1009469376490192896/hW_ZFLHm.jpg",
+  laurynpih: {
+    id: "laurynpih",
+    name: "Lauryn Pih",
+    avatarURL: Lauryn,
     answers: {
       "8xf0y6ziyjabvozdd253nd": "optionOne",
       "6ni6ok3ym7mf1p33lnez": "optionTwo",
@@ -12,22 +24,20 @@ let users = {
     },
     questions: ["8xf0y6ziyjabvozdd253nd", "am8ehyc8byjqgar0jgpub9"],
   },
-  tylermcginnis: {
-    id: "tylermcginnis",
-    name: "Tyler McGinnis",
-    avatarURL:
-      "https://pbs.twimg.com/profile_images/732087375786999808/Ua3m5sd8_400x400.jpg",
+  erikdavidkov: {
+    id: "erikdavidkov",
+    name: "Erik Davidkov",
+    avatarURL: Erik,
     answers: {
       vthrdm985a262al8qx3do: "optionOne",
       xj352vofupe1dqz9emx13r: "optionTwo",
     },
     questions: ["loxhs1bqm25b708cmbf3g", "vthrdm985a262al8qx3do"],
   },
-  johndoe: {
-    id: "johndoe",
-    name: "John Doe",
-    avatarURL:
-      "https://www.cleveland.com/resizer/za1NQ0xd7e3IvuyHgR4e-RiH6m8=/1280x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/image.cleveland.com/home/cleve-media/width2048/img/ent_impact_home/photo/joey-diazjpeg-972f797c791758a2.jpeg",
+  joemagnussen: {
+    id: "joemagnussen",
+    name: "Joe Magnussen",
+    avatarURL: Joe,
     answers: {
       xj352vofupe1dqz9emx13r: "optionOne",
       vthrdm985a262al8qx3do: "optionTwo",
@@ -35,15 +45,92 @@ let users = {
     },
     questions: ["6ni6ok3ym7mf1p33lnez", "xj352vofupe1dqz9emx13r"],
   },
+    calstangroom: {
+    id: "calstangroom",
+    name: "Cal Stangroom",
+    avatarURL: Cal,
+    answers: {
+      xj352vofupe1dqz9emx13r: "optionOne",
+      vthrdm985a262al8qx3do: "optionTwo",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
+    },
+    questions: ["xj352vofupe1dqz9emx13u"],
+  },
+    anniegrey: {
+    id: "anniegrey",
+    name: "Annie Grey",
+    avatarURL: Annie,
+    answers: {
+      xj352vofupe1dqz9emx13r: "optionOne",
+      vthrdm985a262al8qx3do: "optionTwo",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
+    },
+    questions: ["xj352vofupe1dqz9emx13t"],
+  },
+    michaelpeter: {
+    id: "michaelpeter",
+    name: "Michael Peter",
+    avatarURL: Mike,
+    answers: {
+      xj352vofupe1dqz9emx13r: "optionOne",
+      vthrdm985a262al8qx3do: "optionTwo",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
+    },
+    questions: ["xj352vofupe1dqz9emx13d"],
+  },
+    matthewjamesmcarthur: {
+    id: "matthewjamesmcarthur",
+    name: "Matthew James Mcarthur",
+    avatarURL: Matt,
+    answers: {
+      xj352vofupe1dqz9emx13r: "optionOne",
+      vthrdm985a262al8qx3do: "optionTwo",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
+    },
+    questions: ["xj352vofupe1dqz9emx13c"],
+  },
+    jenniferamy: {
+    id: "jenniferamy",
+    name: "Jennifer Amy",
+    avatarURL: Jen,
+    answers: {
+      xj352vofupe1dqz9emx13r: "optionOne",
+      vthrdm985a262al8qx3do: "optionTwo",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
+    },
+    questions: ["xj352vofupe1dqz9emx13h"],
+  },
+    daniellefurlong: {
+    id: "daniellefurlong",
+    name: "Riv",
+    avatarURL: Danni,
+    answers: {
+      xj352vofupe1dqz9emx13r: "optionOne",
+      vthrdm985a262al8qx3do: "optionTwo",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
+    },
+    questions: ["xj352vofupe1dqz9emx1zz"],
+  },
+    ianfrankdoherty: {
+    id: "ianfrankdoherty",
+    name: "Ian",
+    avatarURL: Ian,
+    answers: {
+      xj352vofupe1dqz9emx13r: "optionOne",
+      vthrdm985a262al8qx3do: "optionTwo",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
+    },
+    questions: ["xj352vofupe1dqz9emx1ff"],
+  },
 };
 
 let questions = {
   "8xf0y6ziyjabvozdd253nd": {
     id: "8xf0y6ziyjabvozdd253nd",
-    author: "sarahedo",
+    author: "laurynpih",
     timestamp: 1467166872634,
     optionOne: {
-      votes: ["sarahedo"],
+      votes: ["laurynpih"],
       text: "have horrible short term memory",
     },
     optionTwo: {
@@ -53,67 +140,158 @@ let questions = {
   },
   "6ni6ok3ym7mf1p33lnez": {
     id: "6ni6ok3ym7mf1p33lnez",
-    author: "johndoe",
+    author: "joemagnussen",
     timestamp: 1468479767190,
     optionOne: {
       votes: [],
       text: "become a superhero",
     },
     optionTwo: {
-      votes: ["johndoe", "sarahedo"],
+      votes: ["joemagnussen", "laurynpih"],
       text: "become a supervillain",
     },
   },
   am8ehyc8byjqgar0jgpub9: {
     id: "am8ehyc8byjqgar0jgpub9",
-    author: "sarahedo",
+    author: "laurynpih",
     timestamp: 1488579767190,
     optionOne: {
       votes: [],
       text: "be telekinetic",
     },
     optionTwo: {
-      votes: ["sarahedo"],
+      votes: ["laurynpih"],
       text: "be telepathic",
     },
   },
   loxhs1bqm25b708cmbf3g: {
     id: "loxhs1bqm25b708cmbf3g",
-    author: "tylermcginnis",
+    author: "erikdavidkov",
     timestamp: 1482579767190,
     optionOne: {
       votes: [],
       text: "be a front-end developer",
     },
     optionTwo: {
-      votes: ["sarahedo"],
+      votes: ["laurynpih"],
       text: "be a back-end developer",
     },
   },
   vthrdm985a262al8qx3do: {
     id: "vthrdm985a262al8qx3do",
-    author: "tylermcginnis",
+    author: "erikdavidkov",
     timestamp: 1489579767190,
     optionOne: {
-      votes: ["tylermcginnis"],
+      votes: ["erikdavidkov"],
       text: "find $50 yourself",
     },
     optionTwo: {
-      votes: ["johndoe"],
+      votes: ["joemagnussen"],
       text: "have your best friend find $500",
     },
   },
   xj352vofupe1dqz9emx13r: {
     id: "xj352vofupe1dqz9emx13r",
-    author: "johndoe",
+    author: "joemagnussen",
     timestamp: 1493579767190,
     optionOne: {
-      votes: ["johndoe"],
-      text: "write JavaScript",
+      votes: ["joemagnussen"],
+      text: "Blue cheese",
     },
     optionTwo: {
-      votes: ["tylermcginnis"],
-      text: "write Swift",
+      votes: ["erikdavidkov"],
+      text: "go Effff yah",
+    },
+  },
+    xj352vofupe1dqz9emx13u: {
+    id: "xj352vofupe1dqz9emx13r",
+    author: "calstangroom",
+    timestamp: 1493579767190,
+    optionOne: {
+      votes: ["joemagnussen", "laurynpih"],
+      text: "Noodle the guitar",
+    },
+    optionTwo: {
+      votes: ["erikdavidkov", "michaelpeter", "joemagnussen"],
+      text: "Slap de bass",
+    },
+  },
+    xj352vofupe1dqz9emx13t: {
+    id: "xj352vofupe1dqz9emx13r",
+    author: "anniegrey",
+    timestamp: 1493579767190,
+    optionOne: {
+      votes: ["joemagnussen", "laurynpih"],
+      text: "Guinea pig called Ronnie",
+    },
+    optionTwo: {
+      votes: ["erikdavidkov"],
+      text: "A guinea pig called Borris",
+    },
+  },
+    xj352vofupe1dqz9emx13d: {
+    id: "xj352vofupe1dqz9emx13d",
+    author: "michaelpeter",
+    timestamp: 1493579767190,
+    optionOne: {
+      votes: ["joemagnussen", "laurynpih", "matthewjamesmcarthur"],
+      text: "Live it upp in Dubai",
+    },
+    optionTwo: {
+      votes: ["erikdavidkov"],
+      text: "Get free Panchos for life",
+    },
+  },
+    xj352vofupe1dqz9emx13r: {
+    id: "xj352vofupe1dqz9emx13r",
+    author: "matthewjamesmcarthur",
+    timestamp: 1493579767190,
+    optionOne: {
+      votes: ["joemagnussen", "laurynpih"],
+      text: "Jager and Oblivion",
+    },
+    optionTwo: {
+      votes: ["erikdavidkov"],
+      text: "Dark rum and the Witcher",
+    },
+  },
+    xj352vofupe1dqz9emx13h: {
+    id: "xj352vofupe1dqz9emx13h",
+    author: "jenniferamy",
+    timestamp: 1493579767190,
+    optionOne: {
+      votes: ["joemagnussen", "erikdavidkov"],
+      text: "Go with ABRSM certification",
+    },
+    optionTwo: {
+      votes: ["michaelpeter"],
+      text: "Go with Rockschool certification",
+    },
+  },
+    xj352vofupe1dqz9emx1zz: {
+    id: "xj352vofupe1dqz9emx13h",
+    author: "daniellefurlong",
+    timestamp: 1493579767190,
+    optionOne: {
+      votes: ["joemagnussen", "erikdavidkov"],
+      text: "A left sleeve",
+    },
+    optionTwo: {
+      votes: ["michaelpeter"],
+      text: "A right sleeve",
+    },
+  },
+    xj352vofupe1dqz9emx1hh: {
+    id: "xj352vofupe1dqz9emx13h",
+    author: "ianfrankdoherty",
+    timestamp: 1493579767190,
+    optionOne: {
+      votes: ["joemagnussen", "erikdavidkov"],
+      text: "When Doves Cry?",
+    },
+    optionTwo: {
+      votes: ["michaelpeter"],
+      text: "Bobbert Ross poppin Rick Astley",
     },
   },
 };
