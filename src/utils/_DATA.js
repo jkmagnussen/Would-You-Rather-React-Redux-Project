2de66034897e4ecd7dc6b624336bb93713fa99cd
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-12-22 17:01:45 +0000

    uplosd

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -9,7 +9,7 @@ import Jen from "../media/Jen.jpg"
 import Danni from "../media/Danni.jpg"
 import Ian from "../media/Ian.jpg"
 
-// Simulated database a
+// Simulated databas
 
 let users = {
   laurynpih: {
