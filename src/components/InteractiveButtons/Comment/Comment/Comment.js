a70d0845a9d1147114c901f749d7750771e49c6d
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2021-07-20 03:43:24 +0100

    runner

--- src/components/InteractiveButtons/Comment/Comment/Comment.js
+++ src/components/InteractiveButtons/Comment/Comment/Comment.js
@@ -19,7 +19,7 @@ class Comment extends React.Component {
       likes: 5,
       viewCommentReply: false,
       commentReplyCount: 3,
-      comment: "This is ann example of text that would fill this status/ caption section. This should be relevant to the blurred images displayed beneath to somehow incentivise a particular choice.",
+      comment: "Thiss s is ann example of text that would fill this status/ caption section. This should be relevant to the blurred images displayed beneath to somehow incentivise a particular choice.",
       commentReplies: "This is an example of text that would fill this status/ caption section. This should be relevant to the blurred images displayed beneath to somehow incentivise a particular choice.",
       starComment: false,
     };
